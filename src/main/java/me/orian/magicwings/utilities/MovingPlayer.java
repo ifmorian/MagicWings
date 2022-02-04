01a687f538a40d88caa416238cd831defeb8a72e
Orian <lol.orian2010@gmail.com>
2022-02-04 10:45:27 +0100

    utility class MovingPlayer

---
+++ src/main/java/me/orian/magicwings/utilities/MovingPlayer.java
@@ -0,0 +1,13 @@
+package me.orian.magicwings.utilities;
+
+import org.bukkit.Location;
+
+import java.util.UUID;
+
+public class MovingPlayer {
+
+    UUID id;
+    Location lastLocation;
+    boolean isMoving;
+
+}
