81b52b17da8ce0a4f0a24142e3f08875a3fe4525
Orian <lol.orian2010@gmail.com>
2022-02-04 01:03:18 +0100

    Setup Controller, Listeners

---
+++ src/main/java/me/orian/magicwings/listeners/JoinListener.java
@@ -0,0 +1,14 @@
+package me.orian.magicwings.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+
+    }
+
+}
